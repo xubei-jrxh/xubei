39387f92763773c673c7f1654bc759b2a2fb3749
王世杰 <943777305@qq.com>
2020-08-19 08:42:31 +0800

    项目基本完成

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import element from './element/index'
+Vue.use(element)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
